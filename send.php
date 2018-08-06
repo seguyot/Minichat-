a6f9c91cf3a31b4489ac4fdf365513ce55a45907
seguyot <40633423+seguyot@users.noreply.github.com>
2018-08-06 11:59:41 +0200

    Add files via upload

--- send.php
+++ send.php
@@ -20,7 +20,7 @@ function get_ip() {
 
 
 //insert dans base de donnée
-$req = $bdd->prepare('INSERT INTO minichat.messages (pseudo, message,date,ip, user_agent) VALUES(?, ?,NOW(), ?,?)');
+$req = $bdd->prepare('INSERT INTO mini_chat_Celine.messages (pseudo, message,date,ip, user_agent) VALUES(?, ?,NOW(), ?,?)');
 $req->execute(array($_POST['pseudo'], $_POST['message'], get_ip(),  $_SERVER['HTTP_USER_AGENT']));
 //creation cookie pseudo
 if (isset ($_POST['pseudo'])){
@@ -33,7 +33,7 @@ $pseudoExist->execute(array($_POST['pseudo'] ) );
 
 if ($pseudoExist->fetchColumn() === '0') {
 
-    $speudoCol = $bdd->prepare('INSERT INTO minichat.users (pseudo, color) VALUES(?, ?)');
+    $speudoCol = $bdd->prepare('INSERT INTO mini_chat_Celinet.users (pseudo, color) VALUES(?, ?)');
     $speudoCol->execute(array($_POST['pseudo'], RandomColor::one()));
    
  };
